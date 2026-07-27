f4cec2fc23e427cd610c63d353c7489fc5dffbba
Sage <Raymondzheng2000@gmail.com>
2026-07-27 12:23:45 -0400

    reset css and added device.ts for device capability detection

---
+++ src/device.ts
@@ -0,0 +1,9 @@
+export const isCoarsePointer = () =>
+    typeof matchMedia !== 'undefined' && matchMedia('(pointer: coarse)').matches;
+
+export const canHover = () =>
+    typeof matchMedia !== 'undefined' && matchMedia('(hover: hover)').matches;
+
+export const isIOS = () =>
+    /iPad|iPhone|iPod/.test(navigator.userAgent) ||
+    (navigator.userAgent.includes('Macintosh') && navigator.maxTouchPoints > 1);
